50b721101fe164926a7134f019148d0e9894fc93
rkeerthi <sunnykeerthi@gmail.com>
2023-06-09 01:35:16 +0530

    added wrappers for FAQs and Jobs

--- src/components/page-layout.tsx
+++ src/components/page-layout.tsx
@@ -2,19 +2,27 @@ import * as React from "react";
 import Site from "../types/Site";
 import Header from "./header";
 import Footer from "./footer";
+import {
+  SearchHeadlessProvider,
+  provideHeadless,
+} from "@yext/search-headless-react";
+import searchConfig from "./searchConfig";
 
 type Props = {
   _site?: Site;
   children?: React.ReactNode;
 };
+const searcher = provideHeadless({ ...searchConfig });
 
 const PageLayout = ({ _site, children }: Props) => {
   return (
-    <div className="min-h-screen">
-      <Header _site={_site} />
-      {children}
-      <Footer _site={_site}></Footer>
-    </div>
+    <SearchHeadlessProvider searcher={searcher}>
+      <div className="min-h-screen">
+        <Header _site={_site} />
+        {children}
+        <Footer _site={_site}></Footer>
+      </div>{" "}
+    </SearchHeadlessProvider>
   );
 };
 
---
+++ src/components/pages/FAQsPage.tsx
@@ -0,0 +1,79 @@
+import {
+  Direction,
+  SortBy,
+  SortType,
+  useSearchActions,
+} from "@yext/search-headless-react";
+import {
+  StandardFacets,
+  ResultsCount,
+  AppliedFilters,
+  Pagination,
+  VerticalResults,
+  LocationBias,
+} from "@yext/search-ui-react";
+import * as React from "react";
+import { useEffect } from "react";
+import FAQCard from "../FAQCard";
+import { SortDropdown } from "../SortDropdown";
+
+const FAQsPage = () => {
+  const searchActions = useSearchActions();
+  const sortConfig: Record<string, { label: string; sortBy: SortBy }> = {
+    alpha_asc: {
+      label: "Name: A-Z",
+      sortBy: {
+        field: "name",
+        direction: Direction.Ascending,
+        type: SortType.Field,
+      },
+    },
+    alpha_desc: {
+      label: "Name: Z-A",
+      sortBy: {
+        field: "name",
+        direction: Direction.Descending,
+        type: SortType.Field,
+      },
+    },
+    relevance: {
+      label: "Relevance",
+      sortBy: {
+        type: SortType.Relevance,
+      },
+    },
+  };
+  useEffect(() => {
+    searchActions.setVertical("faqs");
+    searchActions.executeVerticalQuery();
+  }, []);
+  return (
+    <>
+      <div className="flex">
+        <div className="w-64 shrink-0 mr-5 mt-4">
+          <StandardFacets />
+        </div>
+        <div className="flex-grow">
+          <div className="flex flex-col items-baseline">
+            <div className="flex flex-row gap-2 items-center mb-8 w-full">
+              <ResultsCount />
+              <hr className="flex-1" />
+              <SortDropdown sortConfig={sortConfig} />
+            </div>
+            <AppliedFilters />
+          </div>
+          <VerticalResults
+            CardComponent={FAQCard}
+            customCssClasses={{
+              verticalResultsContainer: `max-w-screen-xl`,
+            }}
+          />
+          <Pagination />
+          <LocationBias />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default FAQsPage;
