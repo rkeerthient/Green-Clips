6d5468e7d3835f91d7f620f99cfee34a8a79b191
rkeerthi <sunnykeerthi@gmail.com>
2023-06-21 02:53:46 +0530

    added location page test

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -27,6 +27,8 @@ import List from "../components/list";
 import PageLayout from "../components/page-layout";
 import StaticMap from "../components/static-map";
 import "../index.css";
+import { FiClock, FiMapPin, FiPhone } from "react-icons/fi";
+import { BsCheck2 } from "react-icons/bs";
 
 /**
  * Required when Knowledge Graph data is used for a template.
@@ -140,33 +142,131 @@ const Location: Template<TemplateRenderProps> = ({
   return (
     <>
       <PageLayout _site={_site}>
-        <Banner name={name} address={address} openTime={openTime}>
-          <div className="bg-white h-40 w-1/5 flex items-center justify-center text-center flex-col space-y-4 rounded-lg">
-            <div className="text-black text-base">Visit Us Today!</div>
-            <Cta
-              buttonText="Get Directions"
-              url="http://google.com"
-              style="primary-cta"
-            />
+        <div className="flex flex-row  bg-[#eeeeee]">
+          <div className="w-1/2 text-center py-44 px-12 bg-white">
+            <h1 className="text-4xl font-bold">Great Clips</h1>
+            <h2 className="text-4xl font-light">Great Clips</h2>
+            <p>Open Today: 9:00am to 7:00pm</p>
           </div>
-        </Banner>
+          <div className="w-1/2 text-center py-44  ">
+            <div className=" w-3/4 mx-auto bg-white py-14 px-8 flex flex-col gap-y-5 space-y-5">
+              <div className="flex flex-col text-center justify-center gap-y-2 ">
+                <div className="flex flex-row items-center justify-center">
+                  <FiMapPin size={60} color="#048554" />
+                  <div className="flex flex-col ">
+                    <p className="text-4xl font-light uppercase">Online</p>
+                    <p className="text-2xl font-bold">Check-in</p>
+                  </div>
+                </div>
+                <div className="w-fit px-8 py-4 border text-xl bg-[#048554] mx-auto">
+                  Check In Now
+                </div>
+              </div>
+              <div className="flex flex-row justify-center px-24">
+                <div className="flex flex-col text-left">
+                  <div className="font-bold">Estimated wait:</div>
+                  <div className="w-3/4 text-sm">
+                    Check in online to add your name to the wait list before you
+                    arrive!
+                  </div>
+                </div>
+                <div className="flex flex-col">
+                  <div className="text-3xl font-bold">17</div>
+                  <div className="text-2xl font-bold">MIN</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
         <div className="centered-container">
           <div className="section">
             <div className="grid grid-cols-3 gap-x-10 gap-y-10">
-              <div className="bg-gray-100 p-5 space-y-12">
-                <Contact address={address} phone={mainPhone}></Contact>
-                {services && <List list={services}></List>}
-              </div>
-              <div className="col-span-2 pt-5 space-y-10">
-                <div>
-                  {hours && <Hours title={"Restaurant Hours"} hours={hours} />}
+              <div>
+                <div className="text-xl font-semibold mb-4">
+                  Hair Salon Info
                 </div>
-                {geocodedCoordinate && (
-                  <StaticMap
-                    latitude={geocodedCoordinate.latitude}
-                    longitude={geocodedCoordinate.longitude}
-                  ></StaticMap>
-                )}
+                <div className="mt-6 flex-flex-col gap-2">
+                  {address.line1} <br />
+                  {address.city}, {address.region} {address.postalCode}
+                </div>
+                <div className="italic">In Safeway Center</div>
+                <div>Get Directions</div>
+                <div className="flex justify-center md:justify-start leading-loose items-center text-base md:text-xl">
+                  <FiPhone />
+                  {mainPhone && (
+                    <span className="ml-2">
+                      {mainPhone
+                        .replace("+1", "")
+                        .replace(/\D+/g, "")
+                        .replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3")}
+                    </span>
+                  )}
+                </div>
+              </div>
+              <div>
+                {hours && <Hours title={"Restaurant Hours"} hours={hours} />}
+              </div>
+              <div>
+                {hours && <Hours title={"Restaurant Hours"} hours={hours} />}
+              </div>
+            </div>
+            <div className="grid grid-cols-3 gap-x-10 gap-y-10 mt-12">
+              <div className=" text-xl font-light">
+                <div className="text-xl font-semibold mb-4">
+                  Haircuts for Everyone
+                </div>
+                <div className="flex flex-col space-y-2  text-lg">
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Haircuts for Men and Women</div>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Haircuts for Kids</div>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Haircuts for Seniors</div>
+                  </div>
+                </div>
+              </div>
+              <div className=" text-xl font-light">
+                <div className=" font-semibold mb-4">Haircuts for Everyone</div>
+                <div className="flex flex-col space-y-2 text-lg">
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Short Style</div>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Long Style</div>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Formal Style</div>
+                  </div>
+                </div>
+              </div>
+              <div className=" text-xl font-light">
+                <div className="text-xl font-semibold mb-4">Hair Services</div>
+                <div className="flex flex-col space-y-2 text-lg">
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Neck Trim</div>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Beard Trim</div>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Bang Trim</div>
+                  </div>
+                  <div className="flex gap-2 items-center">
+                    <BsCheck2 color="#048554" />
+                    <div>Hair Shampoo</div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
@@ -175,5 +275,21 @@ const Location: Template<TemplateRenderProps> = ({
     </>
   );
 };
-
+{
+  /* <div className="bg-gray-100 p-5 space-y-12">
+<Contact address={address} phone={mainPhone}></Contact>
+{services && <List list={services}></List>}
+</div>
+<div className="col-span-2 pt-5 space-y-10">
+<div>
+  {hours && <Hours title={"Restaurant Hours"} hours={hours} />}
+</div>
+{geocodedCoordinate && (
+  <StaticMap
+    latitude={geocodedCoordinate.latitude}
+    longitude={geocodedCoordinate.longitude}
+  ></StaticMap>
+)}
+</div> */
+}
 export default Location;
